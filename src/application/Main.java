917ebec128335e43131fdf373263ab4d52f1d03a
Menkor <Menkor@DESKTOP-TE0OQVV>
2018-09-30 09:32:59 +0200

    Jaja

--- src/application/Main.java
+++ src/application/Main.java
@@ -32,7 +32,7 @@ public class Main extends Application {
 				@Override
 				public void handle(MouseEvent event) {
 					primaryStage.setScene(scene2);
-//					primaryStage.show();
+					primaryStage.show();
 				}
 			});
 			
